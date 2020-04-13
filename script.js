40b46f4d0adeb895b5806f3f78d2e61f00d95ac4
manohar <manumanohar1527@gmail.com>
2020-04-13 20:45:15 +0530

    added initial styles & updated player message

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ const button = document.querySelector("button");
 
 //Gobal variables
 let inplay = false;
+
+messager("click the start button ");
 //Adding the eventListeners to the button 
 button.addEventListener("click", () => {
     if (!inplay) {
         inplay = true;
         button.style.display = "none";
-        start();
+        messager("Click the circles as quickly as you see them");
+        ShowBox();
     }
 })
 
-function start() {
+function ShowBox() {
 
+}
+
+///Function which Updated the content of the class message..
+
+function messager(mes) {
+    message.innerHTML = mes;
 }
